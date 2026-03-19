83912e8e48c6945288bcc8fe815b8133f95ae7db
Salman472 <mds619820@gmail.com>
2026-03-19 19:51:21 +0600

    home banner added

--- src/components/buttons/Arrow.jsx
+++ src/components/buttons/Arrow.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { MdArrowOutward } from 'react-icons/md';
 
 const Arrow = () => {
     return (
-        <div>
-            arrow
+        <div className=' rounded-full p-4 border bg-[#1F1F1F] text-white text-xl font-bold'>
+            <MdArrowOutward />
         </div>
     );
 };
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -5,7 +5,7 @@ import Footer from '../pages/shared/Footer/Footer';
 
 const RootLayout = () => {
     return (
-        <div className=''>
+        <div className='space-y-2'>
             <header><Navbar/></header>
             <main className='max-w-11/12 mx-auto'><Outlet/></main>
             <footer className='max-w-11/12 mx-auto'><Footer/></footer>
