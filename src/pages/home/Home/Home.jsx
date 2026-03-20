63ad1833ce0b7325f0d356ff4054a6ce84e76186
Salman472 <mds619820@gmail.com>
2026-03-20 08:46:10 +0600

    service, companies and serviceCard section created

--- src/pages/home/Home/Home.jsx
+++ src/pages/home/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Works from '../Works/Works';
 import Services from '../Services/Services';
+import Companies from '../Companies/Companies';
+import ServiceCard from '../ServiceCard/ServiceCard';
 
 const Home = () => {
     return (
@@ -9,6 +11,8 @@ const Home = () => {
             <Banner/>
             <Works/>
             <Services/>
+            <Companies/>
+            <ServiceCard/>
         </div>
     );
 };
