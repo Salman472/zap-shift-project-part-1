789e37bc09336ed130c94681611dae49ee2a72cb
Salman472 <mds619820@gmail.com>
2026-03-20 16:14:09 +0600

    an added FAQ section

--- src/pages/home/FAQ/FAQ.jsx
+++ src/pages/home/FAQ/FAQ.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import FAQContent from './FAQContent';
 import FAQCard from './FAQCard';
-
+const faqsPromise=fetch('../../../../public/utilits/faqs.json').then(res=>res.json())
 const FAQ = () => {
     return (
         <div>
             <FAQContent/>
-            <FAQCard/>
+            <FAQCard faqsPromise={faqsPromise}/>
         </div>
     );
 };
--- src/pages/home/FAQ/FAQCard.jsx
+++ src/pages/home/FAQ/FAQCard.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { use } from 'react';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 
-const FAQCard = () => {
+const FAQCard = ({faqsPromise}) => {
+    const faqs=use(faqsPromise)
     return (
-        <div>
-            
+        <div className='space-y-4'>
+            <div className='border space-y-4 p-5 rounded-2xl bg-[#E6F2F3]'>
+            <div className='flex justify-between items-center border-b border-gray-400 pb-4'>
+                <h1 className='text-xl font-semibold '>How does this posture corrector work?</h1>
+                <IoIosArrowUp />
+            </div>
+            <p className='text-[#606060]'>A posture corrector works by providing support and gentle alignment to your shoulders, back, and spine, encouraging you to maintain proper posture throughout the day. Here’s how it typically functions: A posture corrector works by providing support and gentle alignment to your shoulders.</p>
+        </div>
+        <div className='space-y-4'>
+            {
+                faqs.map(faq=><>
+                <div className='flex justify-between items-center rounded-2xl p-4 bg-base-200 shadow-sm'>
+                    <h1>{faq?.question}</h1>
+                <IoIosArrowDown />
+                </div>
+                </>)
+            }
+        </div>
         </div>
     );
 };
